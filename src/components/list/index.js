0444742808b5687ff8918437bbdf9310855cad65
ChenUvi <kun.chen@yintech.cn>
2022-01-06 21:56:21 +0800

    Feat: Chapter3

---
+++ src/components/list/index.js
@@ -0,0 +1,41 @@
+﻿import React from 'react';
+import {
+    ListWrapper,
+    ListItem,
+    List
+} from './style';
+import LazyLoad from "react-lazyload";
+import { getCount } from "../../api/utils";
+
+function RecommendList(props) {
+    return (
+        <ListWrapper>
+            <h1 className="title">推荐歌单</h1>
+            <List>
+                {
+                    props.recommendList.map(item => {
+                        return (
+                            <ListItem key={item.id}>
+                                <div className='img_wrapper'>
+                                    <div className="decorate">
+                                        <LazyLoad placeholder={<img width="100%" height="100%"
+                                            src={require('./music.png')} alt="music" />}
+                                        >
+                                        </LazyLoad>
+                                        <div className="play_count">
+                                            <i className="iconfont play">&#xe885;</i>
+                                            <span className="count">{getCount(item.playCount)}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="desc">{item.name}</div>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        </ListWrapper>
+    )
+}
+
+export default React.memo(RecommendList)
